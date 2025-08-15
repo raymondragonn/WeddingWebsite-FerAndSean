c9e4370041c8c6603ebf4f1191108bc460d69221
Scurvybelt <90409656+Scurvybelt@users.noreply.github.com>
2025-08-14 18:22:33 -0600

    Fotos Para la galeria

--- src/components/PortfolioSectionS2/PortfolioSectionS2.js
+++ src/components/PortfolioSectionS2/PortfolioSectionS2.js
@@ -5,6 +5,20 @@ import pImg3 from '../../images/portfolio/FotoColor2.jpeg'
 import pImg4 from '../../images/portfolio/FotoNegro2.jpeg'
 import pImg5 from '../../images/portfolio/FotoColor3.jpeg'
 import pImg6 from '../../images/portfolio/FotoNegro3.jpeg'
+import pImg7 from '../../images/portfolio/FotoBarco.jpeg'
+import pImg8 from '../../images/portfolio/FotoBarco2.jpeg'
+import pImg9 from '../../images/portfolio/FotoBlancoNegroSemaforo.jpeg'
+import pImg10 from '../../images/portfolio/FotoCowboys.jpeg'
+import pImg11 from '../../images/portfolio/FotoEdificio.jpeg'
+import pImg12 from '../../images/portfolio/FotoElevadorColor.jpeg'
+import pImg13 from '../../images/portfolio/FotoEstadio.jpeg'
+import pImg14 from '../../images/portfolio/FotoRapidos.jpeg'
+import pImg15 from '../../images/portfolio/LasVegas.jpeg'
+import pImg16 from '../../images/portfolio/LasVegasCalle.jpeg'
+import pImg17 from '../../images/portfolio/LasVegasCasino.jpeg'
+import pImg18 from '../../images/portfolio/LasVegasCoche.jpeg'
+import pImg19 from '../../images/portfolio/LasVegasElvis.jpeg'
+import pImg20 from '../../images/portfolio/LasVegasNewYork.jpeg'
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
@@ -27,6 +41,48 @@ const Portfolios = [
     },
     {
         Pimg: pImg6,
+    },
+    {
+        Pimg: pImg7,
+    },
+    {
+        Pimg: pImg8,
+    },
+    {
+        Pimg: pImg9,
+    },
+    {
+        Pimg: pImg10,
+    },
+    {
+        Pimg: pImg11,
+    },
+    {
+        Pimg: pImg12,
+    },
+    {
+        Pimg: pImg13,
+    },
+    {
+        Pimg: pImg14,
+    },
+    {
+        Pimg: pImg15,
+    },
+    {
+        Pimg: pImg16,
+    },
+    {
+        Pimg: pImg17,
+    },
+    {
+        Pimg: pImg18,
+    },
+    {
+        Pimg: pImg19,
+    },
+    {
+        Pimg: pImg20,
     },
 
 ]
